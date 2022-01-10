2850ce1da8d4c85f9683de6eb96bfb17a2a94910
savyasanchi shukla <you@example.com>
2022-01-10 15:04:24 +0530

    Tower turret movement

--- Intermediate/Build/Win64/UE4Editor/Inc/ToonTanks/ToonTanks.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/ToonTanks/ToonTanks.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeToonTanks_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x2165DB1F,
-				0x92FEAA01,
+				0x7D4EB4EF,
+				0x6E274060,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
